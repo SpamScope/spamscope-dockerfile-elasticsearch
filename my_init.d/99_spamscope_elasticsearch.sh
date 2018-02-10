e618478b4f8567ad21671ecc17941ef176338670
Fedele Mantuano <mantuano.fedele@gmail.com>
2018-02-10 16:29:03 +0100

    Splitted Elasticsearch templates

--- my_init.d/99_spamscope_elasticsearch.sh
+++ my_init.d/99_spamscope_elasticsearch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-spamscope-elasticsearch -c elasticsearch template -p ${SPAMSCOPE_PATH}/conf/templates/spamscope.json -n spamscope
+spamscope-elasticsearch -c elasticsearch template -p ${SPAMSCOPE_PATH}/conf/templates/spamscope_mails.json -n spamscope_mails
+spamscope-elasticsearch -c elasticsearch template -p ${SPAMSCOPE_PATH}/conf/templates/spamscope_attachments.json -n spamscope_attachments
 spamscope-elasticsearch -c elasticsearch template -p ${SPAMSCOPE_PATH}/conf/templates/commons.json -n commons
 spamscope-elasticsearch -c elasticsearch replicas -i ".kibana, spamscope_*" -n 0
